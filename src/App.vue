75d4d7f0291814ebe71c686b3e571df43430a3e0
Lucas juliano <lucas.juliano@live.com>
2019-12-04 16:11:15 -0300

    commit estrutural

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "App"
+})
+export default class App extends Vue {}
+</script>
+
